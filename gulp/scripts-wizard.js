7c2f97f41288c9cf27b45b491100c8bfabd9a522
Jean Paul Ruiz <jpruiz114@gmail.com>
2017-06-09 13:51:20 -0400

    Adding minified version of scripts

--- gulp/scripts-wizard.js
+++ gulp/scripts-wizard.js
@@ -9,7 +9,7 @@ module.exports = function(gulp, plugins) {
       .pipe(gulp.dest('dist/'))
       .pipe(plugins.rename('formio.wizard.min.js'))
       // DMS - In order to do a better debug, temporarily comment or remove the following line:
-      //.pipe(plugins.streamify(plugins.uglify()))
+      .pipe(plugins.streamify(plugins.uglify()))
       .pipe(gulp.dest('dist/'));
   };
 };

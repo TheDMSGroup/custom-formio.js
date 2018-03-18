a2e1ec2910156892d9da67f1f84bf2f8ab13ba54
Ivan <iivanoff@thedmsgrp.com>
2018-03-18 09:01:12 +0000

    Update the column component

--- build/components/columns/Columns.spec.js
+++ build/components/columns/Columns.spec.js
@@ -7,10 +7,10 @@ var _index = require('./fixtures/index');
 var _harness = require('../../../test/harness');
 
 describe('Columns Component', function () {
-  it('Should build a columns component', function (done) {
-    _harness.Harness.testCreate(_Columns.ColumnsComponent, _index.components.comp1).then(function (component) {
-      _harness.Harness.testElements(component, 'input[type="text"]', 2);
-      done();
+    it('Should build a columns component', function (done) {
+        _harness.Harness.testCreate(_Columns.ColumnsComponent, _index.components.comp1).then(function (component) {
+            _harness.Harness.testElements(component, 'input[type="text"]', 2);
+            done();
+        });
     });
-  });
 });
